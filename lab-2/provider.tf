45cb8cc7e6e35f4aa0aa0fcd1cd65cc074ce16d8
Nguedjang Kamgang Blondel Aurelie <74322332+Aurelie-Kamgang@users.noreply.github.com>
2025-04-16 12:08:59 +0100

    update main and provider in lab2

--- lab-2/provider.tf
+++ lab-2/provider.tf
@@ -6,3 +6,9 @@ terraform {
     }
   }
   }
+
+# features est obligatoire dans les versions récentes du fournisseur azurerm et doit être présent, même si vide. Ce bloc indique à Terraform que le fournisseur Azure est correctement configuré pour être utilisé.
+provider "azurerm" {
+  features {}
+}
+ 
